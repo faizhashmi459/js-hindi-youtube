58f6bab10ea1c5f271e542f1588e1f7df47c1361
Faiz Rasool <115077221+faizhashmi459@users.noreply.github.com>
2023-11-15 14:53:05 +0000

    Js Basics DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";   //Treat all JS code as newer version
+
+//alert(3+3)    //we are using Node JS, not Browser
+
+// Code readability should be high
+
+let name = "Faiz"
+let age = 26
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not assigned any value
+// symbol => used for uniqueness
+
+
+// object 
+
+console.log(typeof "Faiz");
+console.log(typeof age);
+console.log(typeof null);   // type => Object
+console.log(typeof undefined);  // type => undefined
